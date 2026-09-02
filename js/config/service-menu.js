5373c0ac250fc13f0f151bd559cbbcb4617908b3
hasriandi basir <hasriandibasir80@guru.sd.belajar.id>
2026-09-02 08:48:33 +0800

    Fix formatting in service-menu.js

--- js/config/service-menu.js
+++ js/config/service-menu.js
@@ -78,7 +78,7 @@ export const konfigurasiFitur = {
     { nama: 'Aset Sarana', icon: '🏫', link: 'modules/global-monitoring/global-monitoring.html?fitur=aset-sarana' },
     { nama: 'Master Data', icon: '📝', link: 'modules/global-monitoring/global-monitoring.html?fitur=master-data' },
     { nama: 'Evaluasi Mandiri', icon: '', link: 'modules/global-monitoring/global-monitoring.html?fitur=evaluasi-mandiri' },
-    { nama: 'Data TP', icon: '🎯', link: 'modules/global-monitoring/global-monitoring.html?fitur=data-tp' }
+    { nama: 'Data TP', icon: '🎯', link: 'modules/global-monitoring/global-monitoring.html?fitur=data-tp' },
     { nama: 'KOP ', icon: '🎯', link: 'modules/global-monitoring/global-monitoring.html?fitur=kop' }
   ]
 };
